e6cea3662dec59b14ff7d33fd771498a065ae94b
Kaila Nesbit <87931787+Kaila20@users.noreply.github.com>
2023-11-26 23:16:16 -0500

    Finsihed product section

---
+++ Script.js
@@ -0,0 +1,7 @@
+
+//Header/nav sticky menu, when scrolling down
+const header = document.querySelector("header");
+
+window.addEventListener("scroll",function(){
+    header.classList.toggle ("sticky",window.scrollY>0);
+});
